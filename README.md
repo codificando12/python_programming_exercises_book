5fc3e46deed4c13a4b0a3ee7935b5d690b1ec660
Emilio <planeta.chompi18@gmail.com>
2023-02-27 14:29:20 +0800

    firs program "Hello world"

---
+++ README.md
@@ -0,0 +1,1 @@
+In this repository I will do all python exercises from "Python Programming Exercise, Gently Explained" by Al Sweigart.
---
+++ hello_world.py
@@ -0,0 +1,22 @@
+"""Write a program that, when run, greets the user by printing “Hello, world!” on the screen. 
+Then it prints a message on the screen asking the user to enter their name. The program greets the user 
+by name by printing the “Hello,” followed by the user’s name.
+Let’s use “Alice” as the example name. Your program’s output should look like this:
+
+Hello, world!
+
+What is your name?
+
+Alice
+
+Hello, Alice"""
+
+def run():
+    print("Hello, World")
+    name = input("What's your name?\n ")
+
+    print(f"Hello, {name}")
+
+
+if __name__ == "__main__":
+    run()
