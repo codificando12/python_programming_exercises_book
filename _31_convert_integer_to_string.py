88d3370525f232b03e4ab5698efabbd6ab53993f
Emilio <planeta.chompi18@gmail.com>
2023-08-04 08:10:00 +0800

    starting #31 exercise

---
+++ _31_convert_integer_to_string.py
@@ -0,0 +1,26 @@
+"""Exercise Description
+
+Write a convertIntToStr() function with an integerNum parameter. This function operates similarly to the str() 
+function in that it returns a string form of the parameter. For example, convertIntToStr(42) should return the string '42'. 
+The function doesn’t have to work for floating-point numbers with a decimal point, but it should work for negative integer values.
+
+Avoid using Python’s str() function in your code, as that would do the conversion for you and defeat the purpose of this exercise. 
+However, we use str() with assert statements to check that your convertIntToStr() function works the same as str() for all integers from -10000 to 9999:
+
+for i in range(-10000, 10000):
+
+    assert convertIntToStr(i) == str(i)
+
+Try to write a solution based on the information in this description. If you still have trouble solving this exercise, read the Solution 
+Design and Special Cases and Gotchas sections for additional hints.
+
+Prerequisite concepts: dictionaries, while loops, string concatenation, integer division"""
+
+def convertIntToStr(integerNum):
+    
+    INT_TO_STR = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9"}
+    final_str = ""
+    
+
+if __name__ == "__main__":
+    print(convertIntToStr(10))
